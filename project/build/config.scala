eab9bed29487252c9506e8c4c827c6723b2d35e9
Andrey <andrey.shikov@gmail.com>
2011-03-23 14:39:49 -0700

    updated to 2.0

--- project/build/config.scala
+++ project/build/config.scala
@@ -2,15 +2,13 @@ import sbt._
 
 class CircumflexProject(info: ProjectInfo) extends DefaultWebProject(info) {
 
-    val cxVer = "1.2"
+    val cxVer = "2.0"
     override val jettyPort = 8180
 
     override def libraryDependencies = Set(
         "ru.circumflex" % "circumflex-core" % cxVer % "compile->default",
         "ru.circumflex" % "circumflex-orm" % cxVer % "compile->default",
         "ru.circumflex" % "circumflex-ftl" % cxVer % "compile->default",
-        "ru.circumflex" % "circumflex-md" % cxVer % "compile->default",
-        "ru.circumflex" % "circumflex-docco" % cxVer % "compile->default",
 
         "javax.servlet" % "servlet-api" % "2.5" % "compile->default",
 
