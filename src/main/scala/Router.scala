d2462ebebf591f363e6127e1087fb1428d531eed
Andrey <andrey.shikov@gmail.com>
2011-05-18 18:42:03 +0400

    added freemarker usage

--- src/main/scala/Router.scala
+++ src/main/scala/Router.scala
@@ -5,4 +5,16 @@ import ru.circumflex._, core._, web._, freemarker._
 class MyRouter extends RequestRouter {
 
     get("/") = "<h1>it works!</h1>"
+
+    get("/freemarker") = {
+      object simpleObject {
+        val name = "Joe"
+        val subobj = new Object {
+          val name = "Smith"
+        }
+        override def toString = name + " " + subobj.name
+      }
+      'obj:= simpleObject
+      ftl("index.ftl")
+    }
 }
